633acf551dc4fa3b097607bc3a6083a8586ac978
KOH WEI ZHE <weizhekoh123@1utar.my>
2024-07-14 14:57:57 +0800

    added the level 2.1 design and golem enemy pre stage

---
+++ scripts/Enemy_scripts/HitState.gd
@@ -0,0 +1,21 @@
+extends State
+
+class_name HitState
+
+@export var damageable : Damageable
+@export var dead_state : State
+@export var idle_state : State
+@export var dead_animation_node : String = "dead"
+@export var hit_aniamtion_node : String = "hit"
+
+func _ready():
+	damageable.connect("on_hit", on_damageable_hit)
+
+func on_damageable_hit(node : Node, damage_amount : int):
+	if(damageable.health > 0):
+		emit_signal("interrupt_state", self)
+		playback.travel(hit_aniamtion_node)
+
+	else:
+		emit_signal("interrupt_state", dead_state)
+		playback.travel(dead_animation_node)
--- scripts/Player_scripts/PlayerAttackArea.gd
+++ scripts/Player_scripts/PlayerAttackArea.gd
@@ -7,6 +7,11 @@ extends Area2D
 func _ready():
 	player.connect("facing_direction_changed", _on_player_facing_direction_changed)
 
+func _on_body_entered(body):
+	for child in body.get_children():
+		if child is Damageable:
+			child.hit(damage)
+
 func _on_player_facing_direction_changed(facing_right : bool):
 	if(facing_right):
 		facing_shape_2d.position = facing_shape_2d.facing_right_position
