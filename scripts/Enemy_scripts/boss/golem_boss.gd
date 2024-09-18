c45e5c314c6039aa47c09a7a1f76ff2f03be68fd
quanBong <quanbong99890012@gmail.com>
2024-09-19 00:26:36 +0800

    added some feature

--- scripts/Enemy_scripts/boss/golem_boss.gd
+++ scripts/Enemy_scripts/boss/golem_boss.gd
@@ -4,23 +4,25 @@ extends CharacterBody2D
 @onready var sprite = $Sprite2D
 @onready var progress_bar = $UI/ProgressBar
 @export var character : CharacterBody2D
+@onready var damageable = $Damageable
+
+
 
 var direction : Vector2
 var DEF = 0
  
-var health2 = 100:
-	set(value):
-		health2 = value
-		progress_bar.value = value
-		if value <= 0:
-			progress_bar.visible = false
-			find_child("FiniteStateMachine").change_state("Death")
-		elif value <= progress_bar.max_value / 2 and DEF == 0:
-			DEF = 5
-			find_child("FiniteStateMachine").change_state("ArmorBuff") 
- 
+
+
+func on_damageable_hit(node : Node, damage_amount : int, knockback_direction : Vector2):
+	progress_bar.value = (damageable.health / 250) * 100
+	if damageable.health <= 0:
+		progress_bar.visible = false
+		find_child("FiniteStateMachine").change_state("Death")
+
+
 func _ready():
 	set_physics_process(false)
+	damageable.connect("on_hit", _on_damageable_on_hit)
 
 func _process(_delta):
 	direction = player.position - position
@@ -37,3 +39,11 @@ func _physics_process(delta):
 func take_damage():
 	health2 -= 10 - DEF
  
+
+func _on_damageable_on_hit(node, damage_taken, knockback_direction):
+	if(damageable.health > 0):
+		character.velocity = knockback_speed * knockback_direction
+
+	else:
+		#play death animation
+		get_parent().change_state("death")
---
+++ scripts/Level1_scripts/NPC.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+@export var dialog_lines: Array[String] = ["Hello, traveler!", "Welcome to our village."]
+@onready var dialog_manager = DialogManager
+@onready var player = get_parent().find_child("Player")
+@onready var label = $Label
+
+var player_near = false  # Tracks whether the player is near the NPC
+
+func _ready():
+	pass
+
+# Check for interaction input when the player is near
+func _process(delta):
+	if player_near and Input.is_action_just_pressed("interact"):  # "interact" would be "E" key
+		# Position the dialog box relative to the NPC (e.g., above the NPC)
+		var dialog_position = global_position + Vector2(0, -20)  # Adjust the offset as needed
+		dialog_manager.start_dialog(dialog_position, dialog_lines)
+
+func _on_player_detection_body_entered(body):
+	if body.name == "Player":
+		player_near = true
+		print("Player entered")
+		label.visible = true
+
+
+func _on_player_detection_body_exited(body):
+	if body.name == "Player":
+		player_near = false
+		label.visible = false
+		# Ensure the dialog is closed if the player leaves
+		if dialog_manager.is_dialog_active:
+			dialog_manager.close_dialog()
+		print("Player exited")
