f3d40aa2df92289adacef1613668fadce8287fad
Krinowa <weizhekoh123@gmail.com>
2024-09-21 01:22:40 +0800

    amend spike for chi zhe

--- scripts/Level3_scripts/falling_spikes.gd
+++ scripts/Level3_scripts/falling_spikes.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @export var gravity: int = 1000
 @onready var ray_cast: RayCast2D = $RayCast2D
+@onready var timer = $Timer
 
 # Either stuck on ceiling or on ground
 var is_stuck: bool = true
@@ -33,3 +34,14 @@ func _physics_process(delta):
 func apply_gravity(delta):
 	# Apply gravity to the velocity
 	velocity.y += gravity * delta
+
+
+func _on_area_2d_area_entered(area):
+	if area.get_parent() is Player:
+		Engine.time_scale = 0.5
+		area.get_parent().handle_death()
+		timer.start()
+
+func _on_timer_timeout():
+	Engine.time_scale = 1.0
+	get_tree().reload_current_scene()
